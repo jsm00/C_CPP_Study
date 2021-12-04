c3cbc20ef7793a7514ae34a7490d2a7cb818ea76
unknown <jsd014@naver.com>
2021-12-05 00:42:11 +0900

    modified fourth commit

--- C_Project/bookSort.c
+++ C_Project/bookSort.c
@@ -2,16 +2,15 @@
 #include <string.h>
 #include <stdlib.h>
 
-char **bookSort(char *a[], int N);
+char **bookSort(char *a[], int N);      //사용할 함수 선언
 char **swap(char *a[], int i, int j);
 
 int main()
 {
-    char **sortedBookName;
+    char **sortedBookName; //정리된 배열 선언
     int N,n,l=0;
 
-    char buffer[30]; 
-
+    char buffer[30]; //입력 받은 문자열을 저장할 배열
     
     printf("----------------------------------\n");
     printf("책 정리 프로그램에 오신걸 환영합니다.\n");
@@ -19,7 +18,7 @@ int main()
 
     printf("입력할 책의 갯수를 입력해주세요 : ");
     scanf("%d", &N);
-    char *unsortedBookName[N];
+    char *unsortedBookName[N];  //입력 받은 문자열을 배열로 저장할 문자열 배열
 
     printf("책 제목을 입력해 주세요 : ");
 
@@ -28,10 +27,10 @@ int main()
         
         l = strlen(buffer);
         if (l > 0){
-            /*char* newstrptr = (char*)malloc(sizeof(char) * (l + 1));
-            strcpy(newstrptr, buffer);
-            unsortedBookName[n] = newstrptr;*/
-            unsortedBookName[n] = strdup(buffer);
+            /*char* newstrptr = (char*)malloc(sizeof(char) * (l + 1));      //malloc을 통해 buffer에 저장된 길이만큼 할당
+            strcpy(newstrptr, buffer);      //buffer의 내용을 카피하여 저장
+            unsortedBookName[n] = newstrptr;*/   //그 값을 unsortedBookName에 저장
+            unsortedBookName[n] = strdup(buffer);   //strdup() 함수를 통해 저장
             n++;
         } else {
             break;
@@ -39,10 +38,10 @@ int main()
     }
 
     printf("정리중....\n");
-    sortedBookName = bookSort(unsortedBookName, N);
+    sortedBookName = bookSort(unsortedBookName, N);  //정리하는 함수 호출
     printf("결과 : ");
 
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)         //결과 출력
     {
         printf("%s ", sortedBookName[i]);
     }
@@ -50,22 +49,22 @@ int main()
     return 0;
 }
 
-char **bookSort(char *a[], int N)
+char **bookSort(char *a[], int N)       //책 이름 배열과 책의 갯수를 매개변수로 받아 정렬하는 함수
 {
     for (int i = 0; i < N; i++)
     {
         for (int j = i + 1; j < N; j++)
         {
-            if (strncmp(a[i], a[j], 1) > 0)
+            if (strncmp(a[i], a[j], 1) > 0)     //a[j]가 a[i]의 첫 글자보다 값이 크면 두 요소의 순서를 교환
             {
-                a = swap(a, i, j);
+                a = swap(a, i, j);      //swap함수 호출
             }
         }
     }
     return a;
 }
 
-char **swap(char *a[], int i, int j)
+char **swap(char *a[], int i, int j)  //배열의 요소의 순서를 바꿔주는 함수
 {
     char *temp = a[i];
     a[i] = a[j];
